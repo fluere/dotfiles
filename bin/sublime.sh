3280192ad454713ff64de2657f44081852fa91af
fluere <mon.fluffy.lapin@gmail.com>
2014-10-03 02:51:12 +0900

    Sublime Text のセットアップ時に、CSScomb の旧バージョンを取得する処理を追加

--- bin/sublime.sh
+++ bin/sublime.sh
@@ -5,3 +5,8 @@ cd $(pwd)/settingfiles/SublimeText/
 for file in *; do
   ln -s $(pwd)/$file ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/$file
 done
+
+# Install CSScomb (v1.4)
+cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/
+git clone git://github.com/csscomb/sublime-csscomb.git CSScomb && cd $_
+git checkout 370d76c1f85f0376f874b0cc2a554e39fa81fac4
